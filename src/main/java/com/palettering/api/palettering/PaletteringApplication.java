3596a65ad1ba01f10c9fefccf4532216456eaa4c
may-h <hms9210@naver.com>
2020-03-30 19:21:00 +0900

    [ADD] JPA Auditing 설정

--- src/main/java/com/palettering/api/palettering/PaletteringApplication.java
+++ src/main/java/com/palettering/api/palettering/PaletteringApplication.java
@@ -2,7 +2,9 @@ package com.palettering.api.palettering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PaletteringApplication {
 
