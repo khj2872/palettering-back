8b34dc48ae0d3629616b5a3c82495a29a16b4735
may-h <hms9210@naver.com>
2020-03-30 19:17:32 +0900

    [ADD] 스웨거 설정

---
+++ src/main/java/com/palettering/api/palettering/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.palettering.api.palettering.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+//                .ignoredParameterTypes(ApiIgnore.class)
+                .select()
+                .apis(RequestHandlerSelectors.any()) // 현재 RequestMapping으로 할당된 모든 URL 리스트를 추출
+                .paths(PathSelectors.ant("/api/**")) // 그중 /api/** 인 URL들만 필터링
+                .build();
+    }
+}
