166261d8c914b9f178a985c8a89c9fdcde10c634
dbgusrb12 <yhg8484@naver.com>
2020-06-03 19:01:44 +0900

    유저 계정 공개 여부에 따라 수락인지 수락대기인지 설정

--- src/main/java/com/palettering/api/palettering/follow/service/FollowService.java
+++ src/main/java/com/palettering/api/palettering/follow/service/FollowService.java
@@ -2,6 +2,7 @@ package com.palettering.api.palettering.follow.service;
 
 import com.palettering.api.palettering.follow.domain.Follow;
 import com.palettering.api.palettering.follow.repository.FollowRepository;
+import com.palettering.api.palettering.user.user.domain.User;
 import com.palettering.api.palettering.user.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -25,13 +26,18 @@ public class FollowService {
 
     public Follow createFollow(String uid, String targetUid) {
         // TODO 추후 회원 가입되어 있는 회원인지 체크하고, 회원의 계정 공개 여부 판단하여 status 값 변경 해야함
-
+        User user = userRepository.findById(uid).orElseThrow(IllegalAccessError::new);
+        User targetUser = userRepository.findById(targetUid).orElseThrow(IllegalAccessError::new);
+        int status = 0;
+        if(targetUser.isPublic()) {
+            status = 1;
+        }
         return followRepository.save(
                 Follow
                         .builder()
                         .user(userRepository.findById(uid).orElseThrow(IllegalAccessError::new))
                         .targetUser(userRepository.findById(targetUid).orElseThrow(IllegalAccessError::new))
-                        .status(1)  // 무조건 수락을 가정하고 만듬
+                        .status(status)
                         .build()
         );
 
