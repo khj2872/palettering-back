0ff899efa2f8e5b7ae9eecc8e5a1e78995d39efa
강혁진 <hyeokjinkang@i-bricks.co.kr>
2020-06-16 10:36:37 +0900

    패키지 변경

--- src/main/java/com/palettering/api/PaletteringApplication.java
+++ src/main/java/com/palettering/api/PaletteringApplication.java
@@ -1,4 +1,4 @@
-package com.palettering.api.palettering;
+package com.palettering.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
